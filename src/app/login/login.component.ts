bcf2d7f7b895a4a70e5ed71713679924398d8a5a
unknown <thiernohabibbah176@gmail.com>
2023-11-22 12:40:14 +0100

    formulaire ajout

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import Swal from 'sweetalert2';
+import { UserServiceService } from '../user-service.service';
+import {Router, Route } from '@angular/router';
 
 @Component({
   selector: 'app-login',
@@ -6,5 +9,56 @@ import { Component } from '@angular/core';
   styleUrls: ['./login.component.css']
 })
 export class LoginComponent {
+  myData: any;
 
+  constructor(private route:Router, private userService: UserServiceService) {
+  }
+
+  ngOnInit(): void{
+    this.afficher();
+  }
+
+  formData = {
+    email: '',
+    password:''
+  }
+
+  afficher() {
+    this.userService.userData().subscribe((data) => {
+      this.myData = data;
+      console.log(this.myData)
+    });
+  }
+
+
+  submitFunction(event: Event):void {
+    event.preventDefault();
+    if (this.formData.email != "" && this.formData.password == "passer") {
+      let email = this.formData.email;
+      let password = this.formData.password;
+      // @ts-ignore
+      let userFound = this.myData.find(usersdata => usersdata.email === this.formData.email);
+      if (userFound) {
+        // console.log("le user a ete trouve")
+        this.affichermessage('success', "bienvenu", '')
+        this.route.navigate(['/accueil']);
+      }else {
+        console.log("user pas trouver")
+      }
+
+    } else {
+      this.affichermessage('error','Email ou password incorrect','')
+    }
+
+  }
+
+  affichermessage(icone: any, message: string,user:string) {
+    Swal.fire({
+        position: 'center',
+        icon: icone,
+        title: message +"" +user,
+        showConfirmButton: true,
+        // timer: 1500
+    })
+  }
 }
