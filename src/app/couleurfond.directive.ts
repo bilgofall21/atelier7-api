b95f8ad176f3b5b16e7f98b60a68fdb7996de374
bilgofall21 <papaamadem@gmail.com>
2023-11-22 11:38:55 +0000

    ajout page accueil

---
+++ src/app/couleurfond.directive.ts
@@ -0,0 +1,18 @@
+import { Directive, HostBinding } from '@angular/core';
+
+@Directive({
+  selector: '[appCouleurfond]'
+})
+export class CouleurfondDirective {
+@HostBinding ('style.backgroundColor') backgroundColor : string;
+constructor(){
+  this.backgroundColor = "#650329"
+}
+
+  // @HostBinding ('style.backgroundColor') backgroundColor: string;
+  // constructor() {
+  //   this.backgroundColor = "#457A74"
+  // }
+
+
+}
